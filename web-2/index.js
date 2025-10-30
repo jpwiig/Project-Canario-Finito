52f75744c5293fb26ed204296169aa1f5cfb47c6
jpwiig <echo@jpwiig.dev>
2025-10-30 13:55:24 +0100

    web-2 and haproxy

---
+++ web-2/index.js
@@ -0,0 +1,27 @@
+
+document.addEventListener('DOMContentLoaded', async ()=> {
+    console.log("ready!")
+    console.log(fetchFlag())
+
+    const en = await fetchFlag()
+
+    if(en){
+    alert("FEATURE IS ENABLED!!!")
+    document.getElementById("feat").classList.remove("hidden")
+} else {
+    let out = "<h1> nothin here yet! </h1>"
+
+    document.getElementById("stuff").innerHTML+=out
+}
+    
+})
+
+async function fetchFlag(){
+    const url = 'http://localhost:3000/api/flag'
+const feat = await fetch(url)
+let json = await feat.json()
+console.log(json.enabled)
+return json.enabled
+
+
+}
